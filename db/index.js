3a64e29307128bce70e1884a5a1e76e46e984668
Hao-Han Pan <c.avantin94@gmail.com>
2018-10-12 16:06:58 -0500

    Fix style

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD);
 
 const User = db.define('User', {
