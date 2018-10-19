6cf47cb90a1bd3d86bd7dcbc8d60609e987c377d
Hao-Han Pan <c.avantin94@gmail.com>
2018-10-19 18:29:04 -0500

    Create stub of page

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+//import App from './components/App.jsx';
+
+//ReactDOM.render(<App />, document.getElementById("app"));
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
