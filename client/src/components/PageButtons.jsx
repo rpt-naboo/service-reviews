1c2b9eccb2a1e2792a3e01680b3e469e121d4351
Hao-Han Pan <c.avantin94@gmail.com>
2018-10-31 16:27:24 -0500

    Tweak some more styles

--- client/src/components/PageButtons.jsx
+++ client/src/components/PageButtons.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+  margin-bottom: 10px;
+`;
 
 function PageButtons(props) {
   return (
-    <div>
+    <ButtonWrapper>
       <button onClick={props.decrementPage}>&larr; Previous Page</button>
       <button onClick={props.incrementPage}>Next Page &rarr;</button>
-    </div>
+    </ButtonWrapper>
   );
 }
 
--- client/src/components/ReviewerInfo.jsx
+++ client/src/components/ReviewerInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ReviewerWrapper = styled.li`
+const ReviewerWrapper = styled.div`
   font-style: italic;
 `;
 
